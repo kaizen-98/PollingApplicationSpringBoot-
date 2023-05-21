43978c1c49003942c2d773f6595d04c67959d801
kaizen-98 <ammalnassri@gmail.com>
2023-05-21 11:58:50 +0400

    account creation

--- src/main/java/com/example/PollingApplicationSpringBoot/Controller/AccountController.java
+++ src/main/java/com/example/PollingApplicationSpringBoot/Controller/AccountController.java
@@ -1,17 +1,43 @@
 package com.example.PollingApplicationSpringBoot.Controller;
 
+import com.example.PollingApplicationSpringBoot.Models.Account;
+import com.example.PollingApplicationSpringBoot.ResponseObject.GetAccountResponse;
 import com.example.PollingApplicationSpringBoot.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AccountController {
 
     @Autowired
     AccountService accountService;
+    @RequestMapping("Account/register")
+    public void saveAccount()
+    {
+        createAccount();
+    }
+    @RequestMapping("Account/get")
+    public List<Account> getAccount ()
+    {
+        return accountService.getAccount();
+    }
+
+    @RequestMapping("account/get/{accountId}")
+    public GetAccountResponse createAccount (@PathVariable Long accountId)
+    {
+        return accountService.getAccountById(accountId);
+    }
+
+    public void createAccount() {
+        Account account = new Account();
+        account.setUserName("Ali");
+        account.setPassWord("123abc");
+    }
 
 
 
-
-    
 }
