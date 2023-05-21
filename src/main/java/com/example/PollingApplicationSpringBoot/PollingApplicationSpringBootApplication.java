e39da027f2f724bfe06cb08f6b5b7be2e038e8d6
kaizen-98 <ammalnassri@gmail.com>
2023-05-21 10:24:39 +0400

    crateing the main fils

---
+++ src/main/java/com/example/PollingApplicationSpringBoot/PollingApplicationSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PollingApplicationSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingApplicationSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingApplicationSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PollingApplicationSpringBoot/PollingApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PollingApplicationSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
