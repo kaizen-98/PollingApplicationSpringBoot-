af111a0ac12a7f5de7acd560418b77ff8e84b685
kaizen-98 <ammalnassri@gmail.com>
2023-05-21 17:00:02 +0400

    update polls

--- src/main/java/com/example/PollingApplicationSpringBoot/Controller/PollCreationController.java
+++ src/main/java/com/example/PollingApplicationSpringBoot/Controller/PollCreationController.java
@@ -1,4 +1,29 @@
 package com.example.PollingApplicationSpringBoot.Controller;
 
+import com.example.PollingApplicationSpringBoot.Models.PollCreation;
+import com.example.PollingApplicationSpringBoot.Service.PollCreationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PollCreationController {
+
+    @Autowired
+    PollCreationService pollCreationService;
+
+    @RequestMapping("PollCreation/polls")
+    public void savePollCreation()
+    {
+        createPollCreation();
+    }
+
+
+
+    public void createPollCreation(){
+        PollCreation pollCreation = new PollCreation();
+        pollCreation.setQuestion("d");
+        pollCreation.setChoice("d");
+        pollCreationService.savePollCreation(pollCreation);
+    }
 }
--- src/main/java/com/example/PollingApplicationSpringBoot/Service/PollCreationService.java
+++ src/main/java/com/example/PollingApplicationSpringBoot/Service/PollCreationService.java
@@ -1,12 +1,14 @@
 package com.example.PollingApplicationSpringBoot.Service;
 
-import com.example.PollingApplicationSpringBoot.Models.Account;
+
 import com.example.PollingApplicationSpringBoot.Models.PollCreation;
 import com.example.PollingApplicationSpringBoot.Repository.PollCreationRepository;
+import com.example.PollingApplicationSpringBoot.ResponseObject.GetPollCreationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollCreationService {
@@ -18,5 +20,16 @@ public class PollCreationService {
 
     public void savePollCreation(PollCreation pollCreation) {
         pollCreationRepository.save( pollCreation);
+    }
+
+    public GetPollCreationResponse getPollCreationById(Long PollCreationId){
+        Optional<PollCreation> optionalPollCreation = pollCreationRepository.findById(PollCreationId);
+        if (!optionalPollCreation .isEmpty()) {
+            PollCreation PollCreation = optionalPollCreation.get();
+            GetPollCreationResponse pollCreationResponse = new  GetPollCreationResponse(PollCreation.getQuestion(),PollCreation.getChoice());
+            return pollCreationResponse;
+        } else {
+            return null;
+        }
     }
 }
