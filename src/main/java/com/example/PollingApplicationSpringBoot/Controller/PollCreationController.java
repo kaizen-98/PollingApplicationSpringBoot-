137be4ec499849a0c15fefb709e872c810084381
kaizen-98 <ammalnassri@gmail.com>
2023-05-24 10:39:54 +0400

    adding get

--- src/main/java/com/example/PollingApplicationSpringBoot/Controller/PollCreationController.java
+++ src/main/java/com/example/PollingApplicationSpringBoot/Controller/PollCreationController.java
@@ -1,10 +1,13 @@
 package com.example.PollingApplicationSpringBoot.Controller;
 
+import com.example.PollingApplicationSpringBoot.Models.Account;
 import com.example.PollingApplicationSpringBoot.Models.PollCreation;
 import com.example.PollingApplicationSpringBoot.Service.PollCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PollCreationController {
@@ -17,13 +20,19 @@ public class PollCreationController {
     {
         createPollCreation();
     }
-
+    @RequestMapping("PollCreation/get")
+    public List<PollCreation> getPollCreation ()
+    {
+        return pollCreationService.getPollCreation();
+    }
 
 
     public void createPollCreation(){
         PollCreation pollCreation = new PollCreation();
-        pollCreation.setQuestion("d");
-        pollCreation.setChoice("d");
+        pollCreation.setQuestion("What is your favorite color");
+        pollCreation.setChoice("Red");
+        pollCreation.setChoice("Blue");
+        pollCreation.setChoice("Green");
         pollCreationService.savePollCreation(pollCreation);
     }
 }
