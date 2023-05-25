923be2e59b4eec682f30a688a58d6ab8518d94ef
kaizen-98 <ammalnassri@gmail.com>
2023-05-25 11:05:37 +0400

    PollVotingService

--- src/main/java/com/example/PollingApplicationSpringBoot/Service/PollVotingService.java
+++ src/main/java/com/example/PollingApplicationSpringBoot/Service/PollVotingService.java
@@ -1,4 +1,16 @@
 package com.example.PollingApplicationSpringBoot.Service;
 
+import com.example.PollingApplicationSpringBoot.Models.PollVoting;
+import com.example.PollingApplicationSpringBoot.Repository.PollVotingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PollVotingService {
+
+    public List<PollVoting> getPollVoting(){ return pollVotingRepository.findAll();}
+    @Autowired
+    PollVotingRepository pollVotingRepository;
 }
